a1adfda624f076873b93b51b6c56675531b64db8
Henry <th.saravana@gmail.com>
2019-06-19 05:55:17 +0530

    Builder - async without await

--- src/main/kotlin/coroutines/basics/Builders.kt
+++ src/main/kotlin/coroutines/basics/Builders.kt
@@ -1,20 +1,38 @@
 package coroutines.basics
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-fun main() {
-    launch()
-}
-
-private fun launch() {
-    GlobalScope.launch {
-        repeat(100) {
-            println("Executing $it in ${Thread.currentThread()}")
-            delay(1000L)
+class Launch {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            GlobalScope.launch {
+                repeat(100) {
+                    println("Executing $it in ${Thread.currentThread()}")
+                    delay(1000L)
+                }
+            }
+            println("I am in ${Thread.currentThread()}")
+            Thread.sleep(5000L)
         }
     }
-    println("I am in ${Thread.currentThread()}")
-    Thread.sleep(5000L)
+}
+
+class AsyncWithoutAwait {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            GlobalScope.async {
+                repeat(100) {
+                    println("Executing $it in ${Thread.currentThread()}")
+                    delay(1000L)
+                }
+            }
+            println("I am in ${Thread.currentThread()}")
+            Thread.sleep(5000L)
+        }
+    }
 }
